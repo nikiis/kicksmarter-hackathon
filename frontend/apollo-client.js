f28931d37c91810e819ab39dc4197dd9f28460a5
montea <montvydas.klumbys@gmail.com>
2023-04-07 15:20:47 +0100

    Fixed the bug of nto being able to read environmental variables and thus needing to hardcode the api endpoint

--- frontend/apollo-client.js
+++ frontend/apollo-client.js
@@ -3,7 +3,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
+    uri: process.env.NEXT_PUBLIC_API_ENDPOINT,
     cache: new InMemoryCache(),
 });
 
